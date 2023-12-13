0169d717f715230ea5e09a321ef5a54f1c8d5090
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-12-13 20:46:05 +0530

    parsing data in express.js

--- 03-introduction-to-express.js/app.js
+++ 03-introduction-to-express.js/app.js
@@ -1,21 +1,24 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("always runs!");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-  console.log("product page");
-  res.send("<h1>This is a product page!</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add product</button></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("home page");
   res.send("<h1>Hello express.js world!</h1>");
 });
-
 
 app.listen(3000);
