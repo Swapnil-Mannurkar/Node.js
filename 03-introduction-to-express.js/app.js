9711da8dd7fb80a8ff837fc416827ddac5ac39c6
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-12-13 20:15:30 +0530

    routing using express.js

--- 03-introduction-to-express.js/app.js
+++ 03-introduction-to-express.js/app.js
@@ -1,7 +1,21 @@
-const http = require("http");
-
 const express = require("express");
 
-const server = http.createServer();
+const app = express();
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+  console.log("always runs!");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("product page");
+  res.send("<h1>This is a product page!</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("home page");
+  res.send("<h1>Hello express.js world!</h1>");
+});
+
+
+app.listen(3000);
