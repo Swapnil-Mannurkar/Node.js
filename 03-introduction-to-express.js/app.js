7877de570c4f5b529bf5d81ddb346ba11b6b83f7
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-12-13 21:10:08 +0530

    using express router

--- 03-introduction-to-express.js/app.js
+++ 03-introduction-to-express.js/app.js
@@ -1,24 +1,14 @@
 const express = require("express");
 
+const app = express();
 const bodyParser = require("body-parser");
 
-const app = express();
+const adminPage = require("./routes/admin");
+const shopPage = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add product</button></form>"
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello express.js world!</h1>");
-});
+app.use(adminPage);
+app.use(shopPage);
 
 app.listen(3000);
