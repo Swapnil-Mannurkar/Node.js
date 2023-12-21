130510177d9271466b39dcd2c78d5656c2e4b92e
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-12-21 14:46:21 +0530

    added model to products

--- 05-Models-Views-Controllers/controllers/products.js
+++ 05-Models-Views-Controllers/controllers/products.js
@@ -1,5 +1,4 @@
-const products = [];
-exports.products = products;
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -12,6 +11,7 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
--- 05-Models-Views-Controllers/controllers/shop.js
+++ 05-Models-Views-Controllers/controllers/shop.js
@@ -1,6 +1,7 @@
-const products = require("./products").products;
+const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render("shop", {
     prods: products,
     pageTitle: "Shop",
