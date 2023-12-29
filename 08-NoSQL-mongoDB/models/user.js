8a315af383db69e5d9c0bd83f5b0de22eb69f592
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-12-29 12:25:59 +0530

    add to cart

--- 08-NoSQL-mongoDB/models/user.js
+++ 08-NoSQL-mongoDB/models/user.js
@@ -1,16 +1,37 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
-const sequelize = require('../util/database');
+class User {
+  constructor(username, email, cart) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    const db = getDb();
+    const updatedCart = {
+      items: [{ productId: new mongodb.ObjectId(product._id), quantity: 1 }],
+    };
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new mongodb.ObjectId(userId) });
+  }
+}
 
 module.exports = User;
