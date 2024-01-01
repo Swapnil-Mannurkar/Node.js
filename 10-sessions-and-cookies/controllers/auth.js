c687397edbedeecf079bbe94d50056df56c9230b
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2024-01-01 15:57:18 +0530

    added cookie

--- 10-sessions-and-cookies/controllers/auth.js
+++ 10-sessions-and-cookies/controllers/auth.js
@@ -1,6 +1,14 @@
-exports.login = (req, res, next) => {
+exports.getLogin = (req, res, next) => {
+  const loggedIn = req.get("Cookie").split("=")[1] == "true";
+
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
+    isAuthenticated: loggedIn,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/login");
 };
--- 10-sessions-and-cookies/routes/auth.js
+++ 10-sessions-and-cookies/routes/auth.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const authController = require("../controllers/auth");
 
-router.get("/login", authController.login);
+router.get("/login", authController.getLogin);
+
+router.post("/login", authController.postLogin);
 
 module.exports = router;
