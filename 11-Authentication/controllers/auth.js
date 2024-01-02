c6bd1fb1b77b5db14222764b49cce6e07fd1390d
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2024-01-03 01:32:51 +0530

    added flash error messages

--- 11-Authentication/controllers/auth.js
+++ 11-Authentication/controllers/auth.js
@@ -3,31 +3,42 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
+        req.flash("error", "Invalid email or password.");
         return res.redirect("/login");
       }
-
       bcrypt
         .compare(password, user.password)
         .then((doMatch) => {
@@ -35,11 +46,11 @@ exports.postLogin = (req, res, next) => {
             req.session.isLoggedIn = true;
             req.session.user = user;
             return req.session.save((err) => {
-              console.log("ERROR =>", err);
+              console.log(err);
               res.redirect("/");
             });
           }
-
+          req.flash("error", "Invalid email or password.");
           res.redirect("/login");
         })
         .catch((err) => {
@@ -57,9 +68,12 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash(
+          "error",
+          "E-Mail exists already, please pick a different one."
+        );
         return res.redirect("/signup");
       }
-
       return bcrypt
         .hash(password, 12)
         .then((hashedPassword) => {
@@ -68,14 +82,15 @@ exports.postSignup = (req, res, next) => {
             password: hashedPassword,
             cart: { items: [] },
           });
-
           return user.save();
         })
         .then((result) => {
-          return res.redirect("/login");
+          res.redirect("/login");
         });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
