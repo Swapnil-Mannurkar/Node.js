0398cc84946609c639f99c307922f23a1aff2052
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2024-01-04 02:02:35 +0530

    added validation

--- 14-Validation/routes/auth.js
+++ 14-Validation/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const authController = require("../controllers/auth");
 
@@ -10,7 +11,17 @@ router.get("/signup", authController.getSignup);
 
 router.post("/login", authController.postLogin);
 
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  check("email").isEmail().withMessage("Please enter a valid email!"),
+  check("password")
+    .isLength(6)
+    .withMessage("Please enter atleast 6 character password!"),
+  check("password")
+    .equals("confirmPassword")
+    .withMessage("Password does not matches!"),
+  authController.postSignup
+);
 
 router.post("/logout", authController.postLogout);
 
